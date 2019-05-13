ebf2a42ad7229239bfa478c687390b834e9fafd1
daliabdmalek <49783051+daliabdmalek@users.noreply.github.com>
2019-05-13 15:16:24 +0200

    Updates

---
+++ precache-manifest.76ff3aa4a2aa0346e5276e2498cf0f1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135ce6f77e7e2ede1a5f8efaaba9ad5e",
+    "url": "/React-NavMenu/index.html"
+  },
+  {
+    "revision": "8ffed0e9e541e4da561b",
+    "url": "/React-NavMenu/static/css/main.8e890d6e.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/React-NavMenu/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "8ffed0e9e541e4da561b",
+    "url": "/React-NavMenu/static/js/main.cff11b71.chunk.js"
+  },
+  {
+    "revision": "deabeba5b56b0ffa2a59",
+    "url": "/React-NavMenu/static/js/runtime~main.f83f204c.js"
+  }
+]);
